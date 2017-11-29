d3ff00e6a45beeb63780723fe76666830f03a50e
AlexViy <alexviy@gmail.com>
2017-11-29 17:43:55 +0200

    structure is done.

---
+++ src/scripts/data.js
@@ -0,0 +1,28 @@
+var coursesList = [
+  {
+    provider: "Coursera",
+    provider_link: 'https://www.coursera.org/',
+    logo: "assets/coursera/coursera.png",
+    courses: [
+      {
+        name: "HTML, CSS, and Javascript for Web Developers",
+        course_url: "https://www.coursera.org/learn/html-css-javascript-for-web-developers",
+        logo: "assets/coursera/html_css_js.jpg",
+        status: "70"
+      }
+    ]
+  },
+  {
+    provider: "Coursera",
+    provider_link: 'https://www.coursera.org/',
+    logo: "assets/coursera/coursera.png",
+    courses: [
+      {
+        name: "HTML, CSS, and Javascript for Web Developers",
+        course_url: "https://www.coursera.org/learn/html-css-javascript-for-web-developers",
+        logo: "assets/coursera/html_css_js.jpg",
+        status: "70"
+      }
+    ]
+  }
+];
