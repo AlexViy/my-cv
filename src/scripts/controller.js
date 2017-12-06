f117ce6d91649f570e4e449f6b81f67de6710adc
AlexViy <alexviy@gmail.com>
2017-12-06 14:23:10 +0200

    Wrapped codeblock in function

--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -5,9 +5,13 @@ window.onload = function() {
   var providerName = document.getElementsByClassName("provider-name")[0];
   var coursesName = document.getElementsByClassName("courses-name")[0];
 
-  indicator.value = coursesList[0].courses[0].status;
-  providerLink.href = coursesList[0].provider_link;
-  coursesLink.href = coursesList[0].courses[0].course_url;
-  providerName.innerText = coursesList[0].provider;
-  coursesName.innerText = coursesList[0].courses[0].name;
+  function displayData() {
+    indicator.value = coursesList[0].courses[0].status;
+    providerLink.href = coursesList[0].provider_link;
+    coursesLink.href = coursesList[0].courses[0].course_url;
+    providerName.innerText = coursesList[0].provider;
+    coursesName.innerText = coursesList[0].courses[0].name;
+   }
+
+   displayData();
 }
