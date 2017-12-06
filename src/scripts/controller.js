2dfa378e4a7c98951b297f7a7953bd6ecf6d7c07
AlexViy <alexviy@gmail.com>
2017-12-06 13:33:26 +0200

    Changed using tagname to classname.

--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -2,7 +2,7 @@ console.log(coursesList);
 
 
 window.onload = function() {
-  document.getElementsByTagName("progress")[0].value = coursesList[1].courses[0].status
+  document.getElementsByClassName("indicator")[0].value = coursesList[0].courses[0].status
   document.getElementsByClassName("provider-link")[0].href = coursesList[0].provider_link
   document.getElementsByClassName("courses-link")[0].href = coursesList[0].courses[0].course_url
   document.getElementsByClassName("provider-name")[0].innerText = coursesList[0].provider
