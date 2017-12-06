8b8fc5f2939c4258a55164448b2ee2be8b454646
AlexViy <alexviy@gmail.com>
2017-12-06 14:11:01 +0200

    Assigned variables; deleted console.log; added semicolons

--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -1,10 +1,13 @@
-console.log(coursesList);
-
-
 window.onload = function() {
-  document.getElementsByClassName("indicator")[0].value = coursesList[0].courses[0].status
-  document.getElementsByClassName("provider-link")[0].href = coursesList[0].provider_link
-  document.getElementsByClassName("courses-link")[0].href = coursesList[0].courses[0].course_url
-  document.getElementsByClassName("provider-name")[0].innerText = coursesList[0].provider
-  document.getElementsByClassName("courses-name")[0].innerText = coursesList[0].courses[0].name
+  var indicator = document.getElementsByClassName("indicator")[0];
+  var providerLink = document.getElementsByClassName("provider-link")[0];
+  var coursesLink = document.getElementsByClassName("courses-link")[0];
+  var providerName = document.getElementsByClassName("provider-name")[0];
+  var coursesName = document.getElementsByClassName("courses-name")[0];
+
+  indicator.value = coursesList[0].courses[0].status;
+  providerLink.href = coursesList[0].provider_link;
+  coursesLink.href = coursesList[0].courses[0].course_url;
+  providerName.innerText = coursesList[0].provider;
+  coursesName.innerText = coursesList[0].courses[0].name;
 }
