d60ca6d4adefb361b85dc3e0bc25e24bb59560ce
AlexViy <alexviy@gmail.com>
2017-12-07 16:57:35 +0200

    Implemented the same functionality in different way

--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -3,15 +3,30 @@ window.onload = function() {
   var providerLink = document.getElementsByClassName("provider-link")[0];
   var coursesLink = document.getElementsByClassName("courses-link")[0];
   var providerName = document.getElementsByClassName("provider-name")[0];
+  var provider = document.getElementsByClassName("provider")[0];
+  var indicatorContainer = document.getElementsByClassName("indicator-container")[0];
   var coursesName = document.getElementsByClassName("courses-name")[0];
 
   function displayData() {
-    indicator.value = coursesList[0].courses[0].status;
+    // indicator.value = coursesList[0].courses[0].status;
     providerLink.href = coursesList[0].provider_link;
     coursesLink.href = coursesList[0].courses[0].course_url;
     providerName.innerText = coursesList[0].provider;
-    coursesName.innerText = coursesList[0].courses[0].name;
+    // coursesName.innerText = coursesList[0].courses[0].name;
    }
 
    displayData();
+
+   const MAX_PROGRESS_VALUE = 100;
+   var progressBar = document.createElement("progress");
+   var progressBarValue = coursesList[1].courses[0].status;
+   progressBar.setAttribute("value", progressBarValue);
+   progressBar.setAttribute("max", MAX_PROGRESS_VALUE);
+   indicatorContainer.appendChild(progressBar);
+
+   var coursesName = document.createElement("div");
+   coursesName.setAttribute("class", "courses-name")
+   var courseTitle = document.createTextNode(coursesList[0].courses[0].name);
+   coursesName.appendChild(courseTitle);
+   coursesLink.appendChild(coursesName);
 }
