f60c9ce3e58d9e854958c1047538ca25f3a77a2e
AlexViy <alexviy@gmail.com>
2017-12-09 00:13:41 +0200

    Added some data to database

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -28,24 +28,24 @@ var coursesList = [
         course_url: "https://www.codeschool.com/courses/try-python",
         logo: "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/596/try-python-d2bdf771d5ce179c7b198432f349d481.png",
         status: "100"
+      },
+      {
+        name: "Flying Through Python",
+        course_url: "https://www.codeschool.com/courses/flying-through-python",
+        logo: "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/600/flying-through-python-a7c45e7b86f55b47ab2b6d62fc81012c.png",
+        status: "100"
       }
     ]
   },
   {
-    provider: "CodeSchool",
-    provider_link: 'https://www.codeschool.com/',
-    logo: "https://codeschool-discuss.s3-us-west-2.amazonaws.com/original/3X/5/5/55803fb8fb313f83ecd18f45af67e23e7adb34fb.png",
+    provider: "Udacity",
+    provider_link: 'https://www.udacity.com/',
+    logo: "https://d125fmws0bore1.cloudfront.net/assets/udacity_share-317a7f82552763598a5c91e1550b7cd83663ce02d6d475d703e25a873cd3b574.png",
     courses: [
       {
-        name: "Front End Foundaitions",
-        course_url: "https://www.codeschool.com/courses/front-end-foundations",
-        logo: "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/448/course-complete-81fb30908f005de5c75e5376d041f05f.png",
-        status: "100"
-      },
-      {
-        name: "Try Python",
-        course_url: "https://www.codeschool.com/courses/try-python",
-        logo: "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/596/try-python-d2bdf771d5ce179c7b198432f349d481.png",
+        name: "Intro to JavaScript",
+        course_url: "https://www.udacity.com/course/intro-to-javascript--ud803",
+        logo: "https://d125fmws0bore1.cloudfront.net/assets/udacity_share-317a7f82552763598a5c91e1550b7cd83663ce02d6d475d703e25a873cd3b574.png",
         status: "100"
       }
     ]
