619801b93e9f8d10430074869ae9fe5d4140440f
AlexViy <alexviy@gmail.com>
2017-12-10 00:26:58 +0200

    Added data to database

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -51,40 +51,89 @@ var coursesList = [
     ]
   },
   {
-    provider: "CodeSchool",
-    provider_link: 'https://www.codeschool.com/',
-    logo: "https://codeschool-discuss.s3-us-west-2.amazonaws.com/original/3X/5/5/55803fb8fb313f83ecd18f45af67e23e7adb34fb.png",
+    provider: "FreeCodeCamp",
+    provider_link: 'https://www.freecodecamp.org/',
+    logo: "https://avatars0.githubusercontent.com/u/9892522?s=400&v=4",
     courses: [
       {
-        name: "Front End Foundaitions",
-        course_url: "https://www.codeschool.com/courses/front-end-foundations",
-        logo: "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/448/course-complete-81fb30908f005de5c75e5376d041f05f.png",
+        name: "HTML5 and CSS",
+        course_url: "https://www.freecodecamp.org/map-aside#nested-collapseHTML5andCSS",
+        logo: "https://i.ytimg.com/vi/__IJKMzilQo/hqdefault.jpg",
         status: "100"
       },
       {
-        name: "Try Python",
-        course_url: "https://www.codeschool.com/courses/try-python",
-        logo: "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/596/try-python-d2bdf771d5ce179c7b198432f349d481.png",
+        name: "Responsive Design With Bootstrap",
+        course_url: "https://www.freecodecamp.org/map-aside#nested-collapseResponsiveDesignwithBootstrap",
+        logo: "https://i.ytimg.com/vi/2QCPy522Afw/maxresdefault.jpg",
+        status: "100"
+      },
+      {
+        name: "jQuery",
+        course_url: "https://www.freecodecamp.org/map-aside#nested-collapsejQuery",
+        logo: "https://i.ytimg.com/vi/o3pZhyLfRY0/maxresdefault.jpg",
+        status: "100"
+      },
+      {
+        name: "Basic JavaScript",
+        course_url: "https://www.freecodecamp.org/map-aside#nested-collapseBasicJavaScript",
+        logo: "https://i.ytimg.com/vi/DM9o0eZCA8A/maxresdefault.jpg",
+        status: "85"
+      }
+    ]
+  },
+  {
+    provider: "w3schools",
+    provider_link: 'https://www.w3schools.com/',
+    logo: "https://vignette.wikia.nocookie.net/howtoprogram/images/c/ce/W3Schools.png/revision/latest?cb=20130601232401",
+    courses: [
+      {
+        name: "HTML5 Tutorial",
+        course_url: "https://www.w3schools.com/html/default.asp",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/768px-HTML5_logo_and_wordmark.svg.png",
         status: "100"
       }
     ]
   },
   {
-    provider: "CodeSchool",
-    provider_link: 'https://www.codeschool.com/',
-    logo: "https://codeschool-discuss.s3-us-west-2.amazonaws.com/original/3X/5/5/55803fb8fb313f83ecd18f45af67e23e7adb34fb.png",
+    provider: "SoloLearn",
+    provider_link: 'https://www.sololearn.com/',
+    logo: "http://www.thedailystar.net/sites/default/files/styles/big_6/public/feature/images/sololearn.jpg?itok=Gqt21bHB",
     courses: [
       {
-        name: "Front End Foundaitions",
-        course_url: "https://www.codeschool.com/courses/front-end-foundations",
-        logo: "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/448/course-complete-81fb30908f005de5c75e5376d041f05f.png",
+        name: "HTML Fundamentals",
+        course_url: "https://www.sololearn.com/Course/HTML/",
+        logo: "https://www.sololearn.com/Icons/Courses/1014.png",
         status: "100"
       },
       {
-        name: "Try Python",
-        course_url: "https://www.codeschool.com/courses/try-python",
-        logo: "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/596/try-python-d2bdf771d5ce179c7b198432f349d481.png",
+        name: "CSS Fundamentals",
+        course_url: "https://www.sololearn.com/Course/CSS/",
+        logo: "https://www.sololearn.com/Icons/Courses/1023.png",
         status: "100"
+      },
+      {
+        name: "JavaScript Tutorial",
+        course_url: "https://www.sololearn.com/Course/JavaScript/",
+        logo: "https://www.sololearn.com/Icons/Courses/1024.png",
+        status: "100"
+      },
+      {
+        name: "jQuery Tutorial",
+        course_url: "https://www.sololearn.com/Course/jQuery/",
+        logo: "https://www.sololearn.com/Icons/Courses/1082.png",
+        status: "100"
+      },
+      {
+        name: "Python 3 Tutorial",
+        course_url: "https://www.sololearn.com/Course/Python/",
+        logo: "https://www.sololearn.com/Icons/Courses/1073.png",
+        status: "25"
+      },
+      {
+        name: "SQL Fundamentals",
+        course_url: "https://www.sololearn.com/Course/SQL/",
+        logo: "https://www.sololearn.com/Icons/Courses/1060.png",
+        status: "85"
       }
     ]
   }
