6b6fac6375133c1de877be3913f01864f3f6c768
AlexViy <alexviy@gmail.com>
2017-12-14 19:07:57 +0200

    Created controller for adding content

--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -1,8 +1,24 @@
 window.onload = function() {
 
-  document.getElementsByClassName("provider-name")[0].innerText = certificatesList[0].provider
-  document.getElementsByClassName("cert-prov-logo")[0].src = certificatesList[0].logo
-
+  var sraka = document.getElementsByClassName("certificatesList")[0];
+  console.log(sraka);
   coursesAdder(".courses", coursesList);
 
+  var temp = '';
+  for (i = 0; i < certificatesList.length; i++) {
+    temp +=
+      '<div class="provider">' +
+        '<h2 class="provider-name">' +certificatesList[i].provider+'</h2>' +
+        '<ul>';
+        for (j = 0; j < certificatesList[i].certificates.length; j++) {
+          temp +=
+          '<li>' + '<img src="'+certificatesList[i].logo+'" class="cert-prov-logo">' +
+            '<a class="cert-name" href="'+certificatesList[i].certificates[j].url+'" target="_blank">' +
+              certificatesList[i].certificates[j].name +
+            '</a>' +
+          '</li>';
+        }
+    temp += '</ul></div>';
+  }
+  sraka.innerHTML = temp;
 }
