cecfe6e37ce2e5c353bca4d1f70bb89fd5996cd8
AlexViy <alexviy@gmail.com>
2017-12-14 15:08:15 +0200

    Created adder module

--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -1,34 +1,31 @@
 window.onload = function() {
-  var courses = document.querySelector(".courses");
-  var temp = '';
 
-    for (i = 0; i < coursesList.length; i++) {
-      temp +=
-        '<div class="provider">' +
-          '<div class="provider-logo">' +
-            '<img src="'+coursesList[i].logo+'">' +
-          '</div>' +
-            '<a class="provider-link" href="'+coursesList[i].provider_link+'" target="_blank">' +
-              '<h2 class="provider-name">'+coursesList[i].provider+'</h2>'+
-            '</a>' +
-          '<ul>';
-          for (j = 0; j < coursesList[i].courses.length; j++) {
-            temp +=
-              '<li>' +
-                '<div class="courses-logo">'+
-                  '<img src="'+coursesList[i].courses[j].logo+'">' +
-                '</div>'+
-                '<a class="courses-link" href="'+coursesList[i].courses[j].course_url+'" target="_blank">' +
-                  '<div class="courses-name">'+coursesList[i].courses[j].name+'</div>'+
-                '</a>' +
-                '<div class="indicator-container">'+
-                  '<progress value="'+coursesList[i].courses[j].status+'" max="100"></progress>'+
-                '</div>' +
-              '</li>'
-          }
-          temp +=
-            '</ul>' +
-          '</div>';
-      }
-    courses.innerHTML = temp;
+  document.getElementsByClassName("provider-name")[0].innerText = certificatesList[0].provider
+  document.getElementsByClassName("cert-prov-logo")[0].src = certificatesList[0].logo
+
+  coursesAdder(".courses", coursesList);
+
+//   var certificates = document.querySelector(certificates);
+//   var temp = '';
+//   for (i = 0; i < certificatesList.length; i++) {
+//     temp +=
+//       '<div class="provider"' +
+//         '<h2 class="provider_name">''</h2>'
+//       '<ul>';
+//       for (var i = 0; i < certificates.length; i++) {
+//         temp +=
+//           '<li>' +
+//             '<div class="courses-logo">'+
+//               '<img src="'+data[i].courses[j].logo+'">' +
+//             '</div>'+
+//             '<a class="courses-link" href="'+data[i].courses[j].course_url+'" target="_blank">' +
+//               '<div class="courses-name">'+data[i].courses[j].name+'</div>'+
+//             '</a>'+
+//           '</li>'
+//         }
+//         temp +=
+//           '</ul>' +
+//         '</div>';
+//   }
+//
 }
