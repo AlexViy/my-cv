a7f8fe21b7bffe43ab9a2d73af1cc28161ce0b11
AlexViy <alexviy@gmail.com>
2017-12-14 15:34:46 +0200

    Put back changes after merge

--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -1,5 +1,8 @@
 window.onload = function() {
 
+  document.getElementsByClassName("provider-name")[0].innerText = certificatesList[0].provider
+  document.getElementsByClassName("cert-prov-logo")[0].src = certificatesList[0].logo
+
   coursesAdder(".courses", coursesList);
 
 }
