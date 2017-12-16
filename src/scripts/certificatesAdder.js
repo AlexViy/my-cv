b59c0c28a970facd2dc0948c43d01e5f04f87492
AlexViy <alexviy@gmail.com>
2017-12-16 22:32:02 +0200

    Fixed bugs

--- src/scripts/certificatesAdder.js
+++ src/scripts/certificatesAdder.js
@@ -5,17 +5,17 @@ var certificatesAdder = function(target, data) {
   for (i = 0; i < data.length; i++) {
     temp +=
       '<div class="provider">' +
-        '<h2 class="provider-name">' +certificatesList[i].provider+'</h2>' +
+        '<h2 class="provider-name">' +data[i].provider+'</h2>' +
         '<ul>';
-        for (j = 0; j < certificatesList[i].certificates.length; j++) {
+        for (j = 0; j < data[i].certificates.length; j++) {
           temp +=
-          '<li>' + '<img src="'+certificatesList[i].logo+'" class="cert-prov-logo">' +
-            '<a class="cert-name" href="'+certificatesList[i].certificates[j].url+'" target="_blank">' +
-              certificatesList[i].certificates[j].name +
+          '<li>' + '<img src="'+data[i].logo+'" class="cert-prov-logo">' +
+            '<a class="cert-name" href="'+data[i].certificates[j].url+'" target="_blank">' +
+              data[i].certificates[j].name +
             '</a>' +
           '</li>';
         }
     temp += '</ul></div>';
   }
   certificates.innerHTML = temp;
-}
+ }
--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -1,6 +1,25 @@
 window.onload = function() {
 
   coursesAdder(".courses", coursesList);
-  certificatesAdder(".certificate-list", certificatesList);
+  certificatesAdder(".certificates-list", certificatesList);
 
-}
+  // var certificates = document.getElementsByClassName("certificates-list")[0];
+  // var temp = '';
+  //
+  // for (i = 0; i < certificatesList.length; i++) {
+  //   temp +=
+  //     '<div class="provider">' +
+  //       '<h2 class="provider-name">' +certificatesList[i].provider+'</h2>' +
+  //       '<ul>';
+  //       for (j = 0; j < certificatesList[i].certificates.length; j++) {
+  //         temp +=
+  //         '<li>' + '<img src="'+certificatesList[i].logo+'" class="cert-prov-logo">' +
+  //           '<a class="cert-name" href="'+certificatesList[i].certificates[j].url+'" target="_blank">' +
+  //             certificatesList[i].certificates[j].name +
+  //           '</a>' +
+  //         '</li>';
+  //       }
+  //   temp += '</ul></div>';
+  // }
+  // certificates.innerHTML = temp;
+ }
