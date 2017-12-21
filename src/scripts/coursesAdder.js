553f4cee171085ceee8ce9ff333d525e40c6c0c7
AlexViy <alexviy@gmail.com>
2017-12-21 17:09:50 +0200

    Put provider inside col

--- src/scripts/coursesAdder.js
+++ src/scripts/coursesAdder.js
@@ -4,31 +4,33 @@ var coursesAdder = function(target, data) {
 
   for (i = 0; i < data.length; i++) {
     temp +=
-      '<div class="provider col">' +
-        '<div class="provider-logo">' +
-          '<img src="'+data[i].logo+'">' +
-        '</div>' +
-          '<a class="provider-link" href="'+data[i].provider_link+'" target="_blank">' +
-            '<span class="provider-name">'+data[i].provider+'</span>'+
-          '</a>' +
-        '<ul>';
-        for (j = 0; j < data[i].courses.length; j++) {
+      '<div class="col">' +
+        '<div class="provider">' +
+          '<div class="provider-logo">' +
+            '<img src="'+data[i].logo+'">' +
+          '</div>' +
+            '<a class="provider-link" href="'+data[i].provider_link+'" target="_blank">' +
+              '<span class="provider-name">'+data[i].provider+'</span>'+
+            '</a>' +
+          '<ul>';
+          for (j = 0; j < data[i].courses.length; j++) {
+            temp +=
+              '<li>' +
+                '<div class="courses-logo">'+
+                  '<img src="'+data[i].courses[j].logo+'">' +
+                '</div>'+
+                '<a class="courses-link" href="'+data[i].courses[j].course_url+'" target="_blank">' +
+                  '<div class="courses-name">'+data[i].courses[j].name+'</div>'+
+                '</a>' +
+                '<div class="indicator-container">'+
+                  '<progress value="'+data[i].courses[j].status+'" max="100"></progress>'+
+                '</div>' +
+              '</li>'
+          }
           temp +=
-            '<li>' +
-              '<div class="courses-logo">'+
-                '<img src="'+data[i].courses[j].logo+'">' +
-              '</div>'+
-              '<a class="courses-link" href="'+data[i].courses[j].course_url+'" target="_blank">' +
-                '<div class="courses-name">'+data[i].courses[j].name+'</div>'+
-              '</a>' +
-              '<div class="indicator-container">'+
-                '<progress value="'+data[i].courses[j].status+'" max="100"></progress>'+
-              '</div>' +
-            '</li>'
-        }
-        temp +=
-          '</ul>' +
+            '</ul>' +
+          '</div>' +
         '</div>';
-    }
+  }
   courses.innerHTML = temp;
 }
