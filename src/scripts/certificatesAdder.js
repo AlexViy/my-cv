928d6d87791e9be87cfd75e907995ee8edf425d5
AlexViy <alexviy@gmail.com>
2017-12-21 17:18:50 +0200

    Put provider in col div

--- src/scripts/certificatesAdder.js
+++ src/scripts/certificatesAdder.js
@@ -5,23 +5,17 @@ var certificatesAdder = function(target, data) {
   for (i = 0; i < data.length; i++) {
     temp +=
       '<div class="provider col">' +
-        '<div class="provider-info">'+
-          '<img src="'+data[i].logo+'">' +
-          '<span class="provider-name">' +data[i].provider+'</span>' +
-        '</div>' +
-            '<ul>';
-            for (j = 0; j < data[i].certificates.length; j++) {
-              temp +=
-              '<li>' +
-                '<div class="certificates-info">' +
-                  '<img src="'+data[i].certificates[j].icon+'" class="cert-prov-logo">' +
-                    '<a class="cert-name" href="'+data[i].certificates[j].url+'" target="_blank">' +
-                      data[i].certificates[j].name +
-                    '</a>' +
-                '</div>' +
-              '</li>'
-              }
-          temp += '</ul></div>';
+        '<span class="provider-name">' +data[i].provider+'</span>' +
+        '<ul>';
+        for (j = 0; j < data[i].certificates.length; j++) {
+          temp +=
+          '<li>' + '<img src="'+data[i].logo+'" class="cert-prov-logo">' +
+            '<a class="cert-name" href="'+data[i].certificates[j].url+'" target="_blank">' +
+              data[i].certificates[j].name +
+            '</a>' +
+          '</li>';
         }
+    temp += '</ul></div>';
+  }
   certificates.innerHTML = temp;
-}
+ }
