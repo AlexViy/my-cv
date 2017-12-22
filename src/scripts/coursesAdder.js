dbf7a25ad96773d8886affb1dbb18fb495eddd8a
AlexViy <alexviy@gmail.com>
2017-12-22 17:35:34 +0200

    Placed course name into link and aside from logo changed class name

--- src/scripts/coursesAdder.js
+++ src/scripts/coursesAdder.js
@@ -4,27 +4,27 @@ var coursesAdder = function(target, data) {
 
   for (i = 0; i < data.length; i++) {
     temp +=
-      '<div class="col">' +
-        '<div class="courses-provider">' +
-          '<img src="'+data[i].logo+'">' +
-          '<a class="provider-link" href="'+data[i].provider_link+'" target="_blank">' +
-            '<span class="provider-name">'+data[i].provider+'</span>'+
-          '</a>' +
-        '</div>' +
+      '<div class="col col-12 ">' +
+        '<a class="provider-link" href="'+data[i].provider_link+'" target="_blank">' +
+          '<div class="courses-provider">' +
+            '<img src="'+data[i].logo+'">' +
+              '<span class="provider-name">'+data[i].provider+'</span>'+
+        '</a>' +
+      '</div>' +
         '<ul>';
           for (j = 0; j < data[i].courses.length; j++) {
             temp +=
               '<li>' +
-                '<div class="courses-logo">'+
+              '<a class="courses-link" href="'+data[i].courses[j].course_url+'" target="_blank">' +
+                '<div class="courses-info">'+
                   '<img src="'+data[i].courses[j].logo+'">' +
+                  '<span class="courses-name">'+data[i].courses[j].name+'</span>'+
                 '</div>'+
-                '<a class="courses-link" href="'+data[i].courses[j].course_url+'" target="_blank">' +
-                  '<div class="courses-name">'+data[i].courses[j].name+'</div>'+
-                '</a>' +
-                '<div class="progress" style="height: 2px;">' +
-                  '<div class="progress-bar" role="progressbar" style="width: '+data[i].courses[j].status+'%;" aria-valuenow="'+data[i].courses[j].status+'" aria-valuemin="0" aria-valuemax="100"></div>' +
-                '</div>' +
-              '</li>'         
+              '</a>' +
+              '<div class="progress" style="height: 2px;">' +
+                '<div class="progress-bar" role="progressbar" style="width: '+data[i].courses[j].status+'%;" aria-valuenow="'+data[i].courses[j].status+'" aria-valuemin="0" aria-valuemax="100"></div>' +
+              '</div>' +
+            '</li>'
         }
         temp +=
           '</ul>' +
