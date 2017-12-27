c5a50b884d8d56c8b72d70b86a7e64e9a7437de1
AlexViy <alexviy@gmail.com>
2017-12-27 17:37:15 +0200

    Inicial logic idea sketch

--- src/scripts/coursesAdder.js
+++ src/scripts/coursesAdder.js
@@ -1,16 +1,19 @@
-var coursesAdder = function(target, data) {
-  var courses = document.querySelector(target);
-  var temp = '';
+var coursesAdder = function(target1, target2, data) {
+  var courses = document.querySelector(target1);
+  var currLearn = document.querySelector(target2);
+
 
   for (i = 0; i < data.length; i++) {
+    var temp = '';
+
     temp +=
       '<div class="col-12 col-sm-6 col-lg-4 col-xl-3">' +
         '<a class="provider-link" href="'+data[i].provider_link+'" target="_blank">' +
           '<div class="courses-provider">' +
             '<img class="provider-logo" src="'+data[i].logo+'">' +
               '<span class="provider-name">'+data[i].provider+'</span>'+
+          '</div>' +
         '</a>' +
-      '</div>' +
         '<ul>';
           for (j = 0; j < data[i].courses.length; j++) {
             temp +=
@@ -20,16 +23,27 @@ var coursesAdder = function(target, data) {
                   '<img src="'+data[i].courses[j].logo+'">' +
                   '<span class="courses-name">'+data[i].courses[j].name+'</span>'+
                 '</div>'+
-              '</a>' +
-              '<div class="progress" style="height: 2px;">' +
-                '<div class="progress-bar" role="progressbar" style="width: '+data[i].courses[j].status+'%;" aria-valuenow="'+data[i].courses[j].status+'" aria-valuemin="0" aria-valuemax="100"></div>' +
-              '</div>' +
-            '</li>'
+              '</a>';
+              if(data[i].courses[j].status!=='100') {
+                temp +=
+                '<div class="progress" style="height: 2px;">' +
+                  '<div class="progress-bar" role="progressbar" style="width: '+data[i].courses[j].status+'%;" aria-valuenow="'+data[i].courses[j].status+'" aria-valuemin="0" aria-valuemax="100"></div>' +
+                '</div>';
+              }
+            temp += '</li>';
         }
         temp +=
           '</ul>' +
         '</div>' +
       '</div>';
+
+      var emptyDiv = document.createElement('div');
+      emptyDiv.innerHTML = temp;
+      if(temp.indexOf('aria-valuenow="100"')) {
+        courses
+
+      }
+      currLearn.appendChild(emptyDiv);
   }
-  courses.innerHTML = temp;
+
 }
