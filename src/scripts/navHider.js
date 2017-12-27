908be9a43ed990b126c6d18dbc2bbcfd50760d0c
AlexViy <alexviy@gmail.com>
2017-12-27 23:37:25 +0200

    Fixes #37 Created navHIder

---
+++ src/scripts/navHider.js
@@ -0,0 +1,7 @@
+var navHider = function () {
+  var togg = document.querySelector('.navbar-toggler');
+  var collapse = document.querySelector('.navbar-collapse');
+
+  collapse.addEventListener('click', function(){
+    togg.click();
+  })}
