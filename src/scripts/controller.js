c1d77e2c3a079f804949671175ad6d326b2791ce
Alex <alex@ubuntu.ubuntu-domain>
2018-05-26 11:53:06 +0300

    cleaned-up

--- src/scripts/controller.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  coursesAdder(".courses", coursesList);
-  coursesAdder( ".currently-learning", coursesList);
-  certificatesAdder(".certificates-list", certificatesList);
-  navHider();
-});
--- src/scripts/navHider.js
+++
@@ -1,9 +0,0 @@
-var navHider = function () {
-  var togg = document.querySelector('.navbar-toggler');
-  var collapse = document.querySelector('.navbar-collapse');
-
-  collapse.addEventListener('click', function(){
-        if (window.innerWidth < 992) {
-          togg.click();
-        }
-  })}
